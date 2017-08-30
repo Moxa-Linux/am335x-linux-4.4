25f15aba8b56ba37b2347b7fe4d76a31fc792862
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-08-30 21:13:54 +0200

    efi/capsule-loader: Redirect calls to efi_capsule_setup_info() via weak alias
    commit 3fabd628d5ea24b02ddb1230ffca1df0f779f84e upstream.
    
    To allow platform specific code to hook into the capsule loading
    routines, indirect calls to efi_capsule_setup_info() via a weak alias
    of __efi_capsule_setup_info(), allowing platforms to redefine the former
    but still use the latter.
    
    Tested-by: Bryan O'Donoghue <pure.logic@nexus-software.ie>
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-efi@vger.kernel.org
    Link: http://lkml.kernel.org/r/20170602135207.21708-9-ard.biesheuvel@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -129,6 +129,18 @@ typedef struct {
 #define EFI_CAPSULE_POPULATE_SYSTEM_TABLE	0x00020000
 #define EFI_CAPSULE_INITIATE_RESET		0x00040000
 
+struct capsule_info {
+	efi_capsule_header_t	header;
+	int			reset_type;
+	long			index;
+	size_t			count;
+	size_t			total_size;
+	struct page		**pages;
+	size_t			page_bytes_remain;
+};
+
+int __efi_capsule_setup_info(struct capsule_info *cap_info);
+
 /*
  * Allocation types for calls to boottime->allocate_pages.
  */
