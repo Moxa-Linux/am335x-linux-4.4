896b5c741923807446eaa1c1ff25a976ccf99856
Fabrizio Castro <fabrizio.castro@bp.renesas.com>
2018-07-26 18:42:20 +0100

    ARM: shmobile: Remove shmobile_boot_arg from shmobile_smp_apmu_setup_boot
    This commit removes shmobile_boot_arg from shmobile_smp_apmu_setup_boot
    as not used anymore.
    
    Signed-off-by: Fabrizio Castro <fabrizio.castro@bp.renesas.com>
    Reviewed-by: Biju Das <biju.das@bp.renesas.com>
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>

--- arch/arm/mach-shmobile/platsmp-apmu.c
+++ arch/arm/mach-shmobile/platsmp-apmu.c
@@ -186,7 +186,6 @@ static void __init shmobile_smp_apmu_setup_boot(void)
 {
 	/* install boot code shared by all CPUs */
 	shmobile_boot_fn = virt_to_phys(shmobile_smp_boot);
-	shmobile_boot_arg = MPIDR_HWID_BITMASK;
 }
 
 void __init shmobile_smp_apmu_prepare_cpus(unsigned int max_cpus,
