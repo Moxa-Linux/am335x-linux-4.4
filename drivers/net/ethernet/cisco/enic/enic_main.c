cd6b31daf82dc23236fd0f8cfb5479525c9aaf1b
Govindarajulu Varadarajan <gvaradar@cisco.com>
2018-07-30 09:56:54 -0700

    enic: do not call enic_change_mtu in enic_probe
    commit cb5c6568867325f9905e80c96531d963bec8e5ea upstream.
    
    In commit ab123fe071c9 ("enic: handle mtu change for vf properly")
    ASSERT_RTNL() is added to _enic_change_mtu() to prevent it from being
    called without rtnl held. enic_probe() calls enic_change_mtu()
    without rtnl held. At this point netdev is not registered yet.
    Remove call to enic_change_mtu and assign the mtu to netdev->mtu.
    
    Fixes: ab123fe071c9 ("enic: handle mtu change for vf properly")
    Signed-off-by: Govindarajulu Varadarajan <gvaradar@cisco.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [bwh: Backported to 4.4: adjust context]
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>

--- drivers/net/ethernet/cisco/enic/enic_main.c
+++ drivers/net/ethernet/cisco/enic/enic_main.c
@@ -2732,6 +2732,8 @@ static int enic_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	netdev->priv_flags |= IFF_UNICAST_FLT;
 	netdev->mtu = enic->port_mtu;
 
+	netdev->mtu	= enic->port_mtu;
+
 	err = register_netdev(netdev);
 	if (err) {
 		dev_err(dev, "Cannot register net device, aborting\n");
