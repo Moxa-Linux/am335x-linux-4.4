0e8864625024064bd5fd139c2b1ece33ca7430fd
Wes Huang <Wes.Huang@moxa.com>
2019-06-18 20:18:06 +0800

    usb: serial: upgrade mxu11x0 uport driver to version 1.4

--- include/linux/usb/serial/mxu11x0.h
+++ include/linux/usb/serial/mxu11x0.h
@@ -29,7 +29,7 @@
 #define MXU1_ACTIVE_CONFIG			2
 
 /* Vendor and product ids */
-#define MXU1_VENDOR_ID				0x110a	
+#define MXU1_VENDOR_ID				0x110a
 #define TI_VENDOR_ID				0x0451
 #define MXU1_1110_PRODUCT_ID			0x1110
 #define MXU1_1130_PRODUCT_ID			0x1130
